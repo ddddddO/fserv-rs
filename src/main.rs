c4da1dded58d5db2d5520497ead5de4996ea0c98
ddddddO <lbfdeatq@gmail.com>
2023-03-05 13:54:06 +0900

    Return http response

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 /// ref: https://doc.rust-lang.org/std/net/struct.TcpListener.html
 use std::net::{TcpListener, TcpStream};
 use std::io::Result as ioResult;
-use std::io::Write; // なぜこれがいるのか。tcpStreamは既にWrite実装されてるのではないのか
+use std::io::Write; // なぜこれがいるのか。TcpStreamは既にWrite実装されてるのではないのか
+use std::io::Read; // これも
 
 fn main() -> ioResult<()> {
     println!("Lauch fserv-rs.");
@@ -18,13 +19,37 @@ fn main() -> ioResult<()> {
     Ok(())
 }
 
+// ioResult<()> だと、エラー返してないよね？
 fn handle_connection(mut stream: TcpStream) -> ioResult<()> {
     println!("stream: {:?}", stream);
-    stream.write(b"aaaa\nbbb")?;
+
+    read_http(&mut stream)?;
+    write_http(&mut stream)?;
+    // stream.write(b"aaaa\nbbb")?;
     stream.flush()?;
     Ok(())
 }
 
-// fn write_http(mut stream: TcpStream) {
+// TODO: streamの先頭行だけpeekしてプロトコルが何なのかチェックするfunc
 
-// }
+fn read_http(stream: &mut TcpStream) -> ioResult<()> {
+    let mut buf = [0;100]; // FIXME: 固定値やめたい
+    stream.read(&mut buf)?;
+    let s = std::str::from_utf8(&buf).unwrap(); // ここエラーハンドルしないと？
+    println!("req:\n{}", s);
+    Ok(())
+}
+
+fn write_http(stream: &mut TcpStream) -> ioResult<()> {
+    stream.write(b"HTTP/1.1 200 OK\r\n")?;
+    stream.write(b"Allow:: GET\r\n")?; // 可変借用2回以上okだっけ？
+    stream.write(b"Connection: close\r\n")?;
+    stream.write(b"\r\n")?;
+    stream.write(b"\r\n")?;
+
+    stream.write(b"Bodyyyyyyyyy\n")?;
+    stream.write(b"Bodyyyyyyyyy\n")?;
+
+    stream.write(b"\r\n")?;
+    Ok(())
+}
