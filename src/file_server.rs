b14f6dae409655246f0996685f9dba9cfa164f5c
ddddddO <lbfdeatq@gmail.com>
2023-03-11 08:29:51 +0900

    comment

--- src/file_server.rs
+++ src/file_server.rs
@@ -1,6 +1,7 @@
 use std::io::{Result as ioResult};
 
 // TODO: trait側に、HttpServerで実装しているserveを持ってきてデフォルト実装とできないか
+//       いや、共通にするのはまだ待った方がいいかも
 pub trait FileServer {
   fn serve(&self) -> ioResult<()>;
 }
